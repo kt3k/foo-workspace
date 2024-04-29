d8ca8367007cca6546dcd261b0756a321a0f4576
Yoshiya Hinosawa <stibium121@gmail.com>
2024-04-29 19:03:41 +0900

    init

---
+++ foo0/main.ts
@@ -0,0 +1,7 @@
+import { foo1 } from "@kt3k/foo1";
+
+console.log(foo1());
+
+export function foo0(): string {
+  return "foo0";
+}
---
+++ foo1/main.ts
@@ -0,0 +1,7 @@
+import { foo0 } from "@kt3k/foo0";
+
+console.log(foo0());
+
+export function foo1(): string {
+  return "foo1";
+}
